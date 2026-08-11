bc75b791a9fa857a64c78345f3622d19f7c1da2e
ccvillatoror <cc.villatoror@ciencias.unam.mx>
2026-08-11 14:16:14 -0600

    Elimina el debuger.

--- src/application.py
+++ src/application.py
@@ -59,4 +59,4 @@ def create_app():
 application = create_app()
 
 if __name__=="__main__":
-    application.run(debug=True, host='0.0.0.0', port=8000)
+    application.run(host='0.0.0.0', port=8000)
